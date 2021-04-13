8c85aaba42fbb39ba7bf91799db86b9bf4ff7d12
andymck <andrewb.mckenzie@gmail.com>
2021-04-13 15:20:45 +0100

    replace reference definition with input stream PB type

--- priv/grpcbox_service_bhvr.erl
+++ priv/grpcbox_service_bhvr.erl
@@ -8,7 +8,7 @@
 -module({{module_name}}_bhvr).
 
 {{#methods}}
--callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:ctx(), {{pb_module}}:{{input}}(){{/output_stream}}{{/input_stream}}) ->
+-callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:ctx(), {{pb_module}}:{{input}}(){{/output_stream}}{{/input_stream}}) ->
     {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:{{output}}(), ctx:ctx()}{{/output_stream}}{{#output_stream}} | {ok, grpcbox_stream:t()}{{/output_stream}}{{#output_stream}} | {ok, {{pb_module}}:{{output}}(), grpcbox_stream:t()}{{/output_stream}}{{#output_stream}} | {stop, grpcbox_stream:t()}{{/output_stream}}{{#output_stream}} | {stop, {{pb_module}}:{{output}}(), grpcbox_stream:t()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
 {{/methods}}
 
